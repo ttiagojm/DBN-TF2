Added derived regularization term for the gradient step Fixed error on DBNs where Transposed Convs. were using wrong shapes Added Tensorboard Histograms to monitor Weights and Weight Updates Added better shape validations Added Exceptions Fixed Gaussian Dist. Sampling using normalization at the end

DBN/cnn_example.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from utils import get_datasets, get_pretrain_images, get_shallow_net
+from utils import get_datasets, get_shallow_net
 from dbn import DBN
 from crbm import RBMConv
 
@@ -10,7 +10,7 @@ from crbm import RBMConv
 
 # Get dataset original images
 dataset = "cifar10"
-img_train, img_val, img_test, ds_info = get_datasets(dataset, labels=True)
+img_train, img_val, _, ds_info = get_datasets(dataset, 32, labels=True)
 
 # Epochs for shallow nets to train
 epochs = 2
@@ -20,7 +20,7 @@ in_size = ds_info.features["image"].shape
 
 # Kernel and filter size for RBMs
 k_size = 5
-n_filters = 15
+n_filters = 25
 
 print("#### Model trained with original images ####\n")
 
@@ -54,33 +54,33 @@ print("#### Model trained with reconstructed images ####\n")
 
 # Get valid sizes for hidden layer of each RBM (based on kernel size)
 get_hidden_size = lambda input_size, k_size: input_size - k_size + 1
-#first_hidden_size = get_hidden_size(in_size[0], k_size)
-#sec_hidden_size = get_hidden_size(first_hidden_size, k_size)
+first_hidden_size = get_hidden_size(in_size[0], k_size)
+sec_hidden_size = get_hidden_size(first_hidden_size, k_size)
 
 pre_train = tf.keras.Sequential(
     [
         DBN(
             [
-                RBMConv(16, n_filters, rbm_type="gauss"),
-                RBMConv(8, 32),
+                RBMConv(first_hidden_size, n_filters, sigma=1.0),
+                RBMConv(sec_hidden_size, n_filters),
             ]
         )
     ]
 )
 
-# Train the DBN model
-img_train = img_train.map(lambda x, y: x).cache().prefetch(tf.data.AUTOTUNE)
-pre_train.layers[0].fit(img_train, epochs=1)
+# Train the DBN model (keep batch as 10, bigger batches will mess with autoencoder estimations)
+img_train = img_train.map(lambda x, y: x).unbatch().cache().batch(10).prefetch(tf.data.AUTOTUNE)
+pre_train.layers[0].fit(img_train, epochs=2)
 
 # Recreate dataset with reconstructed images
-r_img_train, r_img_val, r_img_test = get_pretrain_images(
-    pre_train, dataset, labels=True
+img_train, img_val, _, ds_info = get_datasets(
+    dataset, 32, labels=True, autoencoder=pre_train
 )
 
 ## Create a shallow ConvNet using encoded data
 shallow = get_shallow_net((sec_hidden_size, sec_hidden_size, n_filters))
 
-hist_recon = shallow.fit(r_img_train, validation_data=r_img_val, epochs=epochs)
+hist_recon = shallow.fit(img_train, validation_data=img_val, epochs=epochs)
 
 print(
     "Acc: %.3f"

DBN/crbm.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
-import tensorflow_probability as tfp
 import sys
-
-# Alias to access distributions module
-tfd = tfp.distributions
+from utils import set_tensorboard_weights, write_tensorboard_weights
 
 
 class RBMConv(tf.keras.layers.Layer):
@@ -45,37 +42,69 @@ class RBMConv(tf.keras.layers.Layer):
         self,
         hidden_units: int,
         n_filters: int,
-        rbm_type="binary",
+        sigma=None,
         training=True,
         k=1,
         lr=1e-3,
+        plot_hist=False,
+        const_reg=None,
+        const_sparse=None,
     ):
+
         """
         Args:
             hidden_units (int): Number of hidden units (latent variables)
             n_filters (int): Number of filters (latent groups)
-            rbm_type(str, optional): Distinguish between binary and real valued visible units
-            training(bool, optional): Boolean to know if RBM is able to train or just infer
+            sigma (float, optional): If visible is real-valued set sigma for Gaussian Dist.
+            training (bool, optional): Set layer to fit or infer
             k (int, optional): Number of Gibbs Samplings
             lr (float, optional): Learning rate
+            plot_hist (bool, optional): Set to plot on Tensorboard
+            const_reg (float, optional): Regularization constant (~ [0.01, 0.1])
+            const_sparse (float, optional): Sparse constant (~ [0.9, 0.99])
+
         """
         super(RBMConv, self).__init__()
 
         self.h = tf.Variable(
-            tf.zeros(shape=(hidden_units, hidden_units, n_filters)),
-            trainable=False,
+            tf.zeros(shape=(hidden_units, hidden_units, n_filters))
         )
-        self.b = tf.Variable(tf.zeros(shape=(n_filters,)), trainable=False)
+        self.b = tf.Variable(tf.zeros(shape=(n_filters,)))
 
         self.n_filters = n_filters
         self.lr = lr
         self.k = k
-        self.sigma = 0.1
+        self.sigma = sigma
         self.training = training
+
+        # Regularizer constants
+        self.const_reg = const_reg
+        self.const_sparse = const_sparse
+
+        if const_reg is None or const_sparse is None:
+            print(
+                "\n[!] Regularizer is OFF because const_reg and/or const_sparse were not set\n"
+            )
+
+        # Set activation to sigmoid if binary, otherwise set as Gaussian Dist.
         self.v_activation = (
-            tf.math.sigmoid
-            if rbm_type == "binary"
-            else self.sample_gaussian_prob
+            tf.math.sigmoid if not sigma else self.sample_gaussian_prob
+        )
+
+        # Set Tensorboard writer
+        self.plot_writer = set_tensorboard_weights() if plot_hist else None
+
+        if plot_hist:
+            print(
+                "\n[!] Tensorboard Histograms are ON. Performance will be impacted significantly\n"
+            )
+
+        # Global step for weights histogram
+        tf.summary.experimental.set_step(0)
+
+        # Debug message
+        print(
+            f"This CRBM have visible {'Binary' if sigma is None else 'Gaussian'}"
         )
 
     def build(self, input_shape):
@@ -88,16 +117,19 @@ class RBMConv(tf.keras.layers.Layer):
         """
 
         # Validate if image is square
-        try:
-            if input_shape[1] != input_shape[2]:
-                raise NotImplemented
-        except NotImplemented as e:
-            print(
-                f"[!] Expect [{input_shape[1]},{input_shape[1]}]. Got [{input_shape[1]},{input_shape[2]}]"
-            )
-            sys.exit(-1)
+        tf.Assert(
+            input_shape[1] == input_shape[2],
+            [f"Image with shape {input_shape} is'not square."],
+        )
 
-        self.a = tf.Variable(0.01, trainable=False)
+        # Based on (Hinton, Geoffrey E, 2012) we should initialize visible bias using proportion
+        # he talk about "units" because it is using RBMs, here we apply that to filters number
+        proportion = 1 / input_shape[3]
+        self.a = tf.Variable(
+            tf.fill(
+                (input_shape[3],), tf.math.log(proportion / (1 - proportion))
+            )
+        )
 
         # Sampling N(μ=0, σ=.01) to initialize weights
         # Don't Forget W shape: Nᵥ-Nₕ+1 x Nᵥ-Nₕ+1 x K
@@ -113,25 +145,59 @@ class RBMConv(tf.keras.layers.Layer):
                 ),
                 stddev=0.01,
             ),
-            trainable=False,
         )
 
     def call(self, inputs):
         """Receive input and transform it
 
                 This is the place where we call other functions to calculate conditionals and reconstruct input
+
         Args:
             inputs (tf.Tensor): Input Tensor
+
+        Returns:
+            Tensor: Latent variable (h)
         """
 
-        self.batch_size = tf.shape(inputs)[0]
-        self.v_shape = tf.shape(inputs)
+        self.v_shape = inputs.shape.as_list()
+        self.batch_size = self.v_shape[0]
 
         # Return h as input for next RBM
         if self.training:
             return self.contrastive_divergence(inputs)
 
         return self.sample_binary_prob(self.h_given_v(inputs))
+
+    def _regularizer_grad(self, v):
+        """Symbolic derivation of regularization term presented on
+           (Lee, Honglak and Ekanadham, Chaitanya and Ng, Andrew, 2007)
+
+            This derivative can be wrong and was not tested if regularization
+            done its job.
+
+        Args:
+            v (Tensor): Input tensor
+
+        Returns:
+            Tensor: Regularization term to apply in H bias
+        """
+
+        sig = self.h_given_v(v)
+        grad_sig = sig * (1 - sig)
+        m = self.batch_size * 1.0
+
+        if self.sigma is None:
+            grad = (self.const_reg / (m)) * -sig * grad_sig
+        else:
+            grad = (self.const_reg / (sigma * m)) * -sig * grad_sig
+
+        # Apply over batches
+        batch_grad = tf.reduce_mean(grad, axis=0)
+
+        # Sum over rows/cols
+        filter_grad = tf.reduce_sum(batch_grad, axis=[0, 1])
+
+        return filter_grad
 
     def contrastive_divergence(self, v):
         """Function to approximate the gradient where we have a positive(ϕ⁺) and negative(ϕ⁻) grad.
@@ -140,6 +206,12 @@ class RBMConv(tf.keras.layers.Layer):
         ϕ⁺ = p(h₍₀₎ = 1 | v₍₀₎ ⋅ v₍₀₎
 
         ϕ⁺ - ϕ⁻ is the constrastive divergence which approximate the derivation of maximum log-likelihood
+
+        Args:
+            v (Tensor): Input Tensor
+
+        Returns:
+            Tensor: Latent variable (h)
         """
 
         # Save original image
@@ -181,14 +253,28 @@ class RBMConv(tf.keras.layers.Layer):
 
         # Needs to be divided by batch size because all convolutions where applied in batch size depth
         # Not in their real depth (input channel)
-        self.W.assign_add(
-            self.lr * (grad / tf.cast(self.batch_size, dtype=tf.float32))
-        )
+        grad = self.lr * (grad / (self.batch_size * 1.0))
+
+        # Plot Update and Weights if enabled
+        if self.plot_writer is not None:
+            name = self.__class__.__name__
+            write_tensorboard_weights(
+                self.plot_writer,
+                grad,
+                name + self.v_activation.__name__ + "Update",
+            )
+            write_tensorboard_weights(
+                self.plot_writer,
+                self.W,
+                name + self.v_activation.__name__ + "Weights",
+            )
+
+        self.W.assign_add(grad)
 
         # Because a is a single value bias for the input tensor all init values where summed then subtracted
         # with the sum of current input values
         self.a.assign_add(
-            self.lr * (tf.reduce_mean(v_init - v, axis=[0, 1, 2]))[0]
+            self.lr * (tf.reduce_mean(v_init - v, axis=[0, 1, 2]))
         )
 
         # Same logic as a but here b has n_filter lines so we sum along all axes except the channel one
@@ -196,11 +282,15 @@ class RBMConv(tf.keras.layers.Layer):
             self.lr * (tf.reduce_mean(h_init - h, axis=[0, 1, 2]))
         )
 
+        # Apply regularization if both parameters are set
+        if self.const_reg is not None and self.const_sparse is not None:
+            self.b.assign_sub(self._regularizer_grad(v))
+
         return self.sample_binary_prob(self.h_given_v(v_init))
 
     def v_given_h(self, h):
-        """Function that implements the conditional probability:
-
+        """
+        Function that implements the conditional probability:
             P(v | h) = σ( (W * h) + a )
 
             Because most of times h is smaller than W and because we are mapping the new probability model, H,
@@ -217,16 +307,23 @@ class RBMConv(tf.keras.layers.Layer):
             is replaced by a Gaussian distribution with mean equal to the result of the convolution and the variance
             will be sigma²
 
+        Args:
+            h (Tensor): Latent Variable
+
         Returns:
             Tensor: Tensor of shape [batch_size, Nv, Nv, channels]
+
         """
+
+        # Create a new shape with right batch size
+        shape = [h.shape.as_list()[0]] + self.v_shape[1:]
         return self.v_activation(
             (
                 self.a
                 + tf.nn.conv2d_transpose(
                     h,
                     self.W,
-                    output_shape=self.v_shape,
+                    output_shape=shape,
                     strides=1,
                     padding="VALID",
                 )
@@ -234,8 +331,8 @@ class RBMConv(tf.keras.layers.Layer):
         )
 
     def h_given_v(self, v):
-        """Function that implements the conditional probability:
-
+        """
+        Function that implements the conditional probability:
             P(h | v) = σ( (Wᵏ * v) + bₖ)
 
 
@@ -245,58 +342,68 @@ class RBMConv(tf.keras.layers.Layer):
             If our CRBM is real-valued, it's needed to divide the result by sigma²
 
         Args:
-            Tensor: Tensor of shape [batch_size, Nw, Nw, n_filters]
+            v (Tensor): Input Tensor
 
         Returns:
-            Tensor: Desc
+            Tensor: Latent variable with shape [batch_size, hidden_units, hidden_units, n_filters]
         """
-        return tf.math.sigmoid(
-            (
-                self.b
-                + tf.nn.conv2d(
-                    v,
-                    tf.reverse(self.W, axis=[0, 1]),
-                    strides=1,
-                    padding="VALID",
-                )
-            )
-            / (
-                tf.square(self.sigma)
-                if self.v_activation == self.sample_gaussian_prob
-                else 1.0
+
+        # Keep shape update
+        self.v_shape = v.shape.as_list()
+
+        sigmoid_res = tf.math.sigmoid(
+            self.b
+            + tf.nn.conv2d(
+                v, tf.reverse(self.W, axis=[0, 1]), strides=1, padding="VALID"
             )
         )
 
-    def sample_gaussian_prob(self, mean):
+        return (
+            sigmoid_res
+            if self.sigma is None
+            else sigmoid_res / tf.square(self.sigma)
+        )
+
+    def sample_gaussian_prob(self, x):
         """Function to create and sample from a Multivariate Gaussian distribution
 
             Mean (loc) is the relation bewteen H and V given by the deconvolution
             Standard Deviation (scale_diag) is the sigma²
 
         Args:
-            mean (Tensor): Result of a deconvolution and used as mean to the distribution
+            x (Tensor): Result of a deconvolution and used as x to the distribution
 
         Returns:
             Tensor: New Tensor with all new samples
         """
-        dist = tfd.MultivariateNormalDiag(
-            loc=mean, scale_diag=tf.fill(tf.shape(mean), tf.square(self.sigma))
+
+        # Create a vector to be our mean
+        # mean should be convolution result from v_given_h like shown in
+        # (Lee, Honglak and Ekanadham, Chaitanya and Ng, Andrew, 2007)
+        mean = tf.reshape(x, [-1])
+
+        # Create a vector with our variance to use as stddev (can't use variance as scalar because is not broadcastable)
+        stddev = tf.fill(tf.shape(mean), tf.square(self.sigma))
+
+        # Sampling using Normal distribution (faster than MultivariateNormalDiag)
+        samples = tf.random.normal(tf.shape(mean), mean=mean, stddev=stddev)
+
+        # Normalize values to be between ℝ: [0,1]
+        samples = (samples - tf.reduce_min(samples)) / (
+            tf.reduce_max(samples) - tf.reduce_min(samples)
         )
 
-        gauss = dist.sample()
+        # Reshape to original shape
+        return tf.reshape(samples, tf.shape(x))
 
-        # print(tf.reduce_min(gauss), tf.reduce_max(gauss))
-
-        return gauss
-
-    def sample_binary_prob(self, probs):
+    def sample_binary_prob(self, x):
         """Function that samples from a Bernoulli distribution
 
-            Here we calculate the probability of a certain element of the probs Tensor being selected
+            Here we calculate the probability of a certain element of the x Tensor being selected
             (Uniform Dist. sampling)
 
-            Subtracting this 2 probabilities (probs and its uniform sampling) will give a negative value
-            if probability = 0 (because the given element in probs as a low probability)
+            Subtracting this 2 probabilities (x and its uniform sampling) will give a negative value
+            if probability = 0 (because the given element in x as a low probability)
 
             Else give a positive value, so probability = 1
 
@@ -307,15 +414,15 @@ class RBMConv(tf.keras.layers.Layer):
             With this a binary Tensor will return
 
         Args:
-            probs (Tensor): Tensor with probabilitites [0,1]
+            x (Tensor): Tensor with probabilitites [0,1]
 
         Returns:
             Tensor: Tensor with binary probabilities
         """
 
-        ## We could use: return tf.reshape(tf.map_fn(lambda x: 1. if x > 0.5 else 0., probs), [-1,1])
+        ## We could use: return tf.reshape(tf.map_fn(lambda x: 1. if x > 0.5 else 0., x), [-1,1])
         ## If sigmoid prob. is less then 0.5 is 0 then is 1, but it's more efficient the function below
-        return tf.nn.relu(tf.sign(probs - tf.random.uniform(tf.shape(probs))))
+        return tf.nn.relu(tf.sign(x - tf.random.uniform(tf.shape(x))))
 
     def get_reconstruction(self, x):
         """Function to return the reconstruction of x based on W, a and b learned previously
@@ -327,20 +434,13 @@ class RBMConv(tf.keras.layers.Layer):
             Tensor: Reconstructed Input Tensor
         """
 
-        # Validate if image doesn't has batch dim
-        try:
-            if len(tf.shape(x)) != 4:
-                raise NotImplemented
-        except NotImplemented as e:
-            print(
-                f"[!] Wrong shape! Should be [Batch, Height, Width, Channels]. Got [{tf.shape(x)}]"
-            )
-            sys.exit(-1)
+        # Validate if Tensor is 4-rank
+        tf.Assert(
+            len(a.shape) == 4,
+            ["Tensor must be 4-rank. Got ", a.shape.as_list()],
+        )
 
-        # Needs to be updated for some calculations
-        self.v_shape = tf.shape(x)
-
-        h = self.sample_binary_prob(self.h_given_v(x))
+        h = self.h_given_v(x)
         v = self.v_given_h(h)
 
         # Return reconstructed input reshape to the original shape

DBN/dbn.py
@@ -2,6 +2,7 @@ from crbm import RBMConv
 from rbm import RBMBernoulli
 from utils import show_batch_images
 from tqdm import tqdm
+from exceptions import RBMListEmpty
 import tensorflow as tf
 
 
@@ -15,24 +16,20 @@ class DBN(tf.keras.layers.Layer):
     RBMs are trained separately.
     """
 
-    def __init__(self, rbms: list, learning_decay=.1):
+    def __init__(self, rbms: list):
+
         """
         Args:
-            rbm_type (str): Type of RBM (CRBM or RBM)
-            rbm_num (int): Number of RBM layers
-            decrease_val (int): Value to decrease the size of each hidden layer
-            rbm_params (dict): Parameters for RBM class
+            rbms (list): List with objects of type RBMBernoulli or RBMConv
         """
         super(DBN, self).__init__()
 
         if not rbms:
-            self.rbms = list()
-        else:
-            self.rbms = rbms
-
-        self.learning_decay = learning_decay
+            raise RBMListEmpty
+        self.rbms = rbms
 
     def call(self, inputs, fit=False):
+
         """Function to train DBN
 
         Args:
@@ -58,16 +55,23 @@ class DBN(tf.keras.layers.Layer):
             Tensor: Reconstructed Input Tensor
         """
 
+        # Save original shapes
+        orig_shapes = list()
+
         # Feed forward with Gibbs Sampling
+        # Save shapes for the backwards propagation
         for rbm in self.rbms:
-            rbm.training = False
-            x = rbm(x)
+            # orig_shapes.append(x.shape.as_list())
+            x = rbm.h_given_v(x)
 
         # Here x will be h, because the reconstruction of all RBMs (except first) is the h of the previous RBM
+        # Shapes of each RBM are updated to assure that "Deconvolutions" work well
         for rbm in reversed(self.rbms):
+            # rbm.v_shape = shape
             x = rbm.v_given_h(x)
 
-        return tf.clip_by_value(x, clip_value_min=0., clip_value_max=1.)
+        # Clipping values to ℝ: [0,1]
+        return tf.clip_by_value(x, clip_value_min=0.0, clip_value_max=1.0)
 
     def fit(self, inputs, epochs=1, verbose=False):
         """Function to fit and freeze the model
@@ -84,6 +88,7 @@ class DBN(tf.keras.layers.Layer):
         for epoch in range(epochs):
             print(f"#### Epoch {epoch+1} ####")
 
+            # Reset inputs each epoch
             inputs = orig_input
 
             for rbm in self.rbms:
@@ -95,9 +100,9 @@ class DBN(tf.keras.layers.Layer):
                 # After training, freeze the model
                 rbm.training = False
 
-                # Update learning rate for next epoch
-                rbm.lr *= self.learning_decay
-
-                # Get inputs for the next RBM
-                inputs = inputs.map(lambda x: rbm(
-                    x)).cache().prefetch(tf.data.AUTOTUNE)
+                # Get inputs (h) for the next RBM
+                inputs = (
+                    inputs.map(lambda x: rbm(x))
+                    .cache()
+                    .prefetch(tf.data.AUTOTUNE)
+                )

DBN/utils.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import sys
+import os
+from time import time
 from inspect import signature, _empty
 
 
@@ -12,14 +14,74 @@ normalizer = tf.keras.layers.Rescaling(1.0 / 255)
 unormalizer = lambda x: tf.cast(tf.keras.layers.Rescaling(255)(x), tf.int32)
 
 
-def get_datasets(dataset_name: str, normalize=True, labels=False):
+def preprocess(
+    ds: tf.data.Dataset,
+    batch_size: int,
+    labels: bool,
+    normalize: bool,
+    autoencoder=None,
+):
+    """Function to preprocess data
+
+       Its needed to be able to map the 3 datasets.
+
+    Args:
+        ds (tf.data.Dataset): Dataset to iterate
+        batch_size (int): Number of images per batch
+        labels (bool): Boolean to determine if labels should be returned
+        normalize (bool, optional): Determine if the dataset should or shouldn't be normalized
+        autoencoder (function, optional): Autoencoder function/layer
+
+    Returns:
+        tuple: Tuple of processed train, validation and test data tensors
+    """
+    # Normalize Dataset or just return it
+    norm_or_not = lambda x: normalizer(x) if normalize else x
+
+    # The order changes if autoencoder is passed because autoencoder needs that the Dataset is batched
+    if autoencoder is None:
+        result = (
+            ds.map(
+                lambda x, y: (norm_or_not(x), y) if labels else norm_or_not(x)
+            )
+            .cache()
+            .shuffle(tf.data.experimental.cardinality(ds))
+            .batch(batch_size)
+            .prefetch(tf.data.AUTOTUNE)
+        )
+    else:
+        result = (
+            ds.cache()
+            .shuffle(tf.data.experimental.cardinality(ds))
+            .batch(batch_size)
+            .map(
+                lambda x, y: (autoencoder(norm_or_not(x)), y)
+                if labels
+                else autoencoder(norm_or_not(x))
+            )
+            .prefetch(tf.data.AUTOTUNE)
+        )
+
+    return result
+
+
+def get_datasets(
+    dataset_name: str,
+    batch_size: int,
+    normalize=True,
+    labels=False,
+    autoencoder=None,
+):
     """Function get a dataset and prepare it to be used to efficiently on training pipeline
 
     Args:
         dataset_name (str): Name of the dataset of tensorflow_datasets
+        batch_size (int): Number of images per batch
         normalize (bool, optional): Determine if the dataset should or shouldn't be normalized
+        labels (bool, optional): Boolean to determine if labels should be returned
+        autoencoder (function, optional): Autoencoder function/layer
 
-    Returns:
+    Return:
         tuple: Tuple of processed train, validation and test data tensors
     """
     (img_train, img_val, img_test), ds_info = tfds.load(
@@ -29,100 +91,11 @@ def get_datasets(dataset_name: str, normalize=True, labels=False):
         with_info=True,
     )
 
-    if normalize:
-        norm_train = (
-            img_train.map(
-                lambda x, y: normalizer(x)
-                if not labels
-                else (normalizer(x), y)
-            )
-            .cache()
-            .shuffle(tf.data.experimental.cardinality(img_train))
-            .batch(32)
-            .prefetch(tf.data.AUTOTUNE)
-        )
-        norm_val = (
-            img_val.map(
-                lambda x, y: normalizer(x)
-                if not labels
-                else (normalizer(x), y)
-            )
-            .cache()
-            .shuffle(tf.data.experimental.cardinality(img_val))
-            .batch(32)
-            .prefetch(tf.data.AUTOTUNE)
-        )
-        norm_test = (
-            img_test.map(
-                lambda x, y: normalizer(x)
-                if not labels
-                else (normalizer(x), y)
-            )
-            .cache()
-            .shuffle(tf.data.experimental.cardinality(img_test))
-            .batch(32)
-            .prefetch(tf.data.AUTOTUNE)
-        )
-
-        return norm_train, norm_val, norm_test, ds_info
-
-    return img_train, img_val, img_test, ds_info
-
-
-def get_pretrain_images(autoencoder, dataset_name: str, labels=False):
-    """Function get a dataset and encoded it using an autoencoder
-
-            Dataset will be always normalized!
-
-    Args:
-        autoencoder (Model): Autoencoder model
-        dataset_name (str): Name of the dataset of tensorflow_datasets
-
-    Returns:
-        tuple: Tuple of processed train, validation and test data tensors
-    """
-    (img_train, img_val, img_test), ds_info = tfds.load(
-        dataset_name,
-        split=["train[:80%]", "train[80%:]", "test"],
-        as_supervised=True,
-        with_info=True,
+    norm_train, norm_val, norm_test = map(
+        lambda x: preprocess(x, batch_size, labels, normalize, autoencoder),
+        [img_train, img_val, img_test],
     )
-
-    r_img_train = (
-        img_train.cache()
-        .shuffle(tf.data.experimental.cardinality(img_train))
-        .batch(32)
-        .map(
-            lambda x, y: autoencoder(normalizer(x))
-            if not labels
-            else (autoencoder(normalizer(x)), y)
-        )
-        .prefetch(tf.data.AUTOTUNE)
-    )
-    r_img_val = (
-        img_val.cache()
-        .shuffle(tf.data.experimental.cardinality(img_val))
-        .batch(32)
-        .map(
-            lambda x, y: autoencoder(normalizer(x))
-            if not labels
-            else (autoencoder(normalizer(x)), y)
-        )
-        .prefetch(tf.data.AUTOTUNE)
-    )
-    r_img_test = (
-        img_test.cache()
-        .shuffle(tf.data.experimental.cardinality(img_test))
-        .batch(32)
-        .map(
-            lambda x, y: autoencoder(normalizer(x))
-            if not labels
-            else (autoencoder(normalizer(x)), y)
-        )
-        .prefetch(tf.data.AUTOTUNE)
-    )
-
-    return r_img_train, r_img_val, r_img_test
+    return norm_train, norm_val, norm_test, ds_info
 
 
 """
@@ -139,6 +112,7 @@ def show_batch_images(batch, pred, num_imgs=5, unormalize=True):
         batch (Tensor): Tensor with a batch of original images
         pred (Tensor): Tensor with a batch of reconstructed images
         num_imgs (int, optional): Number of images to show from batch
+        unormalize (bool, optional): Unnormalize images
     """
     for img, img_pred in zip(batch[:num_imgs], pred[:num_imgs]):
 
@@ -156,10 +130,6 @@ def show_batch_images(batch, pred, num_imgs=5, unormalize=True):
 """
 	Helper functions/classes for DBN
 """
-
-# Just a custom except for verify_args function
-class RequiredParamError(Exception):
-    pass
 
 
 def get_shallow_net(in_size: tuple[int]):
@@ -195,3 +165,25 @@ def get_shallow_net(in_size: tuple[int]):
     )
 
     return shallow
+
+
+def set_tensorboard_weights():
+    """Create a Summary Writter
+
+    Returns:
+        tf.summary: Writter to log histograms
+    """
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    return tf.summary.create_file_writer(os.path.join(base_dir, "Tensorboard"))
+
+
+def write_tensorboard_weights(writer, weights: tf.Tensor, name: str):
+    """Write weights to plot on Tensorboard Histograms
+
+    Args:
+        writer (tf.summary): Summary writter
+        weights (tf.Tensor): Tensor with weights
+        name (str): Name of histogram
+    """
+    with writer.as_default():
+        tf.summary.histogram(name, weights)